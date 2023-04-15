8c3bc9277a6bcb7d9c8b2ad7559d8197d7c8a532
Ericksonlees <130806747+Ericksonlees@users.noreply.github.com>
2023-04-14 17:51:44 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,56 @@
+﻿<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+   //s $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'erickaonlee665@gmail.com';                     //SMTP username
+    $mail->Password   = 'Kbnausctzzvntuue';                               //SMTP password
+    $mail->SMTPSecure ='tls';                                          //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('erickaonlee665@gmail.com','LEES ARTS');
+    $mail->addAddress($email, $name);     //Add a recipient
+   $mail->addAddress('erickaonlee665@gmail.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+   // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'LEES ARTS Confirmation email';
+    $mail->Body    = 'Thank you, we received your message: ' . $subject;
+   // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Thank you .Your Message has been sent. We will get back to you soon';
+    header( "Location: index.html");
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+
+}
